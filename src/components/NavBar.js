29ac3f87f6cc090cfdf6ed2f70590b6dc82ade22
kgauzampe <53508326+kgauzampe@users.noreply.github.com>
2026-03-04 10:22:49 +0200

    Nav Updated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,32 @@
-// import { Link } from "react-router-dom";
+import { useState } from "react";
 import "../styles/NavBar.scss";
 
 export default function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
   return (
     <nav className="navbar">
       <div className="navbar__logo">
-        <img 
-          src="/img/logo.png" 
-          alt="Creative Logo" 
-          className="navbar__logo-img"
-        />
+        <img src="/img/logo.png" alt="Creative Logo" />
+        CreativeSpace
       </div>
 
-      <ul className="navbar__links">
-       <li>
-        <a href="#about">About</a>
-      </li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#contact">Contact</a></li>
+      <ul className={`navbar__links ${menuOpen ? "active" : ""}`}>
+        <li><a href="#about" onClick={() => setMenuOpen(false)}>About</a></li>
+        <li><a href="#services" onClick={() => setMenuOpen(false)}>Services</a></li>
+        <li><a href="#contact" onClick={() => setMenuOpen(false)}>Contact</a></li>
       </ul>
+
+      <div
+        className={`navbar__hamburger ${menuOpen ? "active" : ""}`}
+        onClick={toggleMenu}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </nav>
   );
 }
